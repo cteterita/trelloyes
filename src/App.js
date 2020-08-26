63492d6e992068dbb60b17a4137dd486fc0c0e03
Claire Lesh <cteterita@gmail.com>
2020-08-26 12:12:29 -0700

    Added three basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './store.js';
+import List from './composition/List';
 
 function App() {
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main className="App">
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className="App-list">
+        <List header="List 1"></List>
+      </div>
     </main>
   );
 }
---
+++ src/composition/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+function List(props) {
+    return (
+        <section className="List">
+            <header class="List-header">
+                <h2>{props.header}</h2>
+            </header>
+            <div class="List-cards">
+                <Card title="Card1" content="Card 1 Content"></Card>
+            </div>
+        </section>
+      );
+}
+
+export default List;
